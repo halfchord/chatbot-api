864a796f29d5dc8d81a222055b92083335b97557
74705 <747057591@qq.com>
2025-09-05 13:20:09 +0800

    halfchord feat: init ddd project

---
+++ chatbot-api-doman/src/test/java/org/example/chatbot/api/doman/ChatbotApiDomanApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.chatbot.api.doman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatbotApiDomanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ chatbot-api-interfaces/src/test/java/org/example/chatbot/api/interinterfaces/ChatbotApiInterfacesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.chatbot.api.interinterfaces;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatbotApiInterfacesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
